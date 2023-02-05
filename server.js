9a9570a7ead5040d00b897cb041b9634b935f388
Shivam Rajput <shivamrajput@Shivams-MacBook-Air.local>
2023-02-05 01:06:13 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+
+app.get("/", (req, res) =>{
+    res.send("API is Running");
+});
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server started at  ${PORT}"));
